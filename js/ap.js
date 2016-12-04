e0e3a5021791c6dff219a47c52ff23ef2c24bb1e
Jay Berg <jaybny@gmail.com>
2016-12-04 10:22:06 -0800

    Ticker nav

--- js/ap.js
+++ js/ap.js
@@ -235,6 +235,7 @@ function loadNav(){
                 <ul class='nav navbar-nav  navbar-right'>\
                     <li> <a href='/template/download.html' id='App'>Download App</a> </li>\
                     <li> <a href='/template/leaderboard.html'>Leaderboard</a> </li>\
+                    <li> <a href='/template/ticks.html'>Ticker</a> </li>\
                     <li> <a href='/Protoblock-Mobile-Quick-Start-Guide.pdf' id='quick'>Quick Start PDF</a></li> \
                 </ul>\
             </div>\
